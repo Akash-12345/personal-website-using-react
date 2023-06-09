33d198e9ae8c07aff16c862db698654387fbf37c
akashkumarsearce <akash.kumar@searce.com>
2023-06-09 15:41:03 +0530

    changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,6 @@
 import "./intro.css";
 import Me from "../../img/me.png";
+
 const Intro = () => {
   return (
     <div className="i">
@@ -8,18 +9,19 @@ const Intro = () => {
           <h2 className="i-intro">Hello, My name is</h2>
           <h1 className="i-name">Akash AnilKumar</h1>
           <div className="i-title">
-            <h1>I'm a passionate</h1>
+           <h1>I'm a passionate</h1> 
             <div className="i-title-wrapper">
-              <div className="i-title-item">Software Engineer</div>
+              <div className="i-title-item">Software Enginner</div>
               <div className="i-title-item">Web Developer</div>
               <div className="i-title-item">Programmer</div>
             </div>
           </div>
           <p className="i-desc">
-            A resourceful Software engineer proficient in Reactjs, Nodejs, Golang, Python and SQL.
-            <hr className="break" />
-            Graduated from National Institute of Technology Calicut in Electronics and Communication with a score of 81.2% in academics.
-            Continuous learner of new technologies and tools.
+          A resourceful Software engineer 
+          proficient in Reactjs, Nodejs, Golang, Python and SQL. 
+          <hr className="break"/>
+          Graduated from National Institute of Technology Calicut in Electronics and Communication with a score of 81.2% in academics. 
+          Continuous learner of new technologies and tools.
           </p>
         </div>
       </div>
